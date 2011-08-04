e4f5d899a0b2c19550aa6bd63043a1a9cf87ebb8
Paul Miller <jettero@gmail.com>
2011-08-04 19:20:59 -0400

    going to perlize this sloppy module

---
+++ sloppy.pl
@@ -0,0 +1,44 @@
+#!/usr/bin/perl
+
+use common::sense;
+use Inline Config=>DIRECTORY=>"$ENV{HOME}/.ratsloppy_c_";
+use Inline C=>DATA=>LIBS =>"-L/usr/X11R6/lib -lX11";
+
+
+sloppy();
+
+__END__
+__C__
+
+#include <X11/Xos.h>
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+#include <X11/Xproto.h>
+
+int sloppy() {
+    Display *display;
+    int i, numscreens;
+
+    display = XOpenDisplay(NULL);
+
+    if(!display) {
+        perror("could not open display");
+        exit(1);
+    }
+
+    numscreens = ScreenCount(display);
+
+    for (i=0; i<numscreens; i++) {
+        unsigned int j, nwins;
+        Window dw1, dw2, *wins;
+
+        XSelectInput(display,RootWindow(display, i), SubstructureNotifyMask);
+        XQueryTree(display, RootWindow(display, i), &dw1, &dw2, &wins, &nwins);
+        for (j=0; j<nwins; j++)
+            XSelectInput(display, wins[j], EnterWindowMask);
+    }
+
+    XCloseDisplay (display);
+
+    printf("sloppy\n");
+}
